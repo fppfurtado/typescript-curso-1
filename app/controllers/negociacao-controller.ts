a8e5d252c8fa4503f42b4ff78db8df9d72fd083f
Fernando Furtado <fppfurtado@proton.me>
2024-08-20 20:23:02 -0300

    conclusao do curso typescript1

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,42 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes);
+        this.limparFormulario();
+    }
+
+    private criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(
+            data,
+            quantidade,
+            valor
+        );
+        return negociacao;
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = null
+        this.inputQuantidade.value = null
+        this.inputValor.value = null;
+        this.inputData.focus();
+    }
+}

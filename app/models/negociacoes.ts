bd145854034d6a81dbadc80dcfe3d27346169220
Fernando Furtado <fppfurtado@proton.me>
2024-08-21 19:24:33 -0300

    implementa aula 1.6 do curso typescript 2

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,7 +1,7 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Negociacao[];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
